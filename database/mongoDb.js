478e3e8cd45919274893a539cc21c59294f005dd
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2020-08-11 11:41:23 +0700

    Third

--- database/mongoDb.js
+++ database/mongoDb.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-  const uri = 'mongodb://localhost:27017/techbase';
+  const uri = process.env.MONGO_URL;
   const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   };
   await mongoose.connect(uri, options);
-  console.log('Connect mongo success');
+  console.log(`Connect mongo success port:${process.env.MONGO_URL}`);
 }
 
 module.exports = {
